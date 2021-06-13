5e46ed65cd26a435c1c78b90309d1906fc9155d0
alaminrasel <85047672+alaminrasel@users.noreply.github.com>
2021-06-13 10:43:04 +0600

    Create AssessmentTask6.php

---
+++ Lab 2/AssessmentTask6.php
@@ -0,0 +1,13 @@
+        $arr = ["Eggs", "Bacon", "HashBrowns", "Beans", "Bread", "RedSauce"];
+        $arrayLength = count($arr);
+        $search= "Beans"
+        
+        $i = 0;
+        while ($i < $arrayLength)
+        {
+            if($arr[i]==$search)
+            {
+            echo ".$arr[$i] ." "<br />";
+            }
+            $i++;
+        }
