70e285cfe973d4aeb477362c55da33d425038f59
alaminrasel <85047672+alaminrasel@users.noreply.github.com>
2021-06-13 10:14:23 +0600

    Create AssTask1.php

---
+++ Lab 2/AssTask1.php
@@ -0,0 +1,6 @@
+<?php  
+ $length = 15;  
+ $width = 10;  
+ echo "Area of rectangle is $length * $width= " . ($length * $width) . "<br />";  
+ echo "Perimeter of rectangle is $length * $width= " . (2* ($length + $width) ). "<br />";  
+  ?>  
