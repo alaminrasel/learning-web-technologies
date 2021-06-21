fee2e6974aa976ee1415171fdb14197e2e557c4b
alaminrasel <85047672+alaminrasel@users.noreply.github.com>
2021-06-21 23:02:08 +0600

    Create email.php

---
+++ LAB_TASK_4_PHP_FORM/Task 2/A/email.php
@@ -0,0 +1,18 @@
+
+<?php
+	if(isset($_POST['submit'])){
+
+		$email	=  $_POST['email'];
+
+		if($email == ""){
+			echo "Null submission...";
+		}else{
+			echo "Success";
+		}
+
+	}else{
+		echo 'invalid request...';
+	}
+
+
+?>
