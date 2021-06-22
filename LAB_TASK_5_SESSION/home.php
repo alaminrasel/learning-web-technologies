9355acd76217c4c443d6ce9a58179ecbf971a2ac
alaminrasel <85047672+alaminrasel@users.noreply.github.com>
2021-06-23 01:43:44 +0600

    Create home.php

---
+++ LAB_TASK_5_SESSION/home.php
@@ -0,0 +1,15 @@
+  
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Index Page</title>
+</head>
+<body>
+	<center>
+		<a href="home.php">Home</a> |
+		<a href="login.php">Login</a>|
+		<a href="Registration.php">Registration</a>
+		<h1>Welcome To Homepage! </h1>
+	</center>
+</body>
+</html>
